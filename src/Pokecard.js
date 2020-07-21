8b75eb7f52286749cefcc3e9e5e04bfe8da43bb4
github username <github email address>
2020-07-21 01:27:03 -0400

    Add nicer images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 
 class Pokecard extends Component {
   
+  padToThree = (number) => {
+    return number <= 999 ? `00${number}`.slice(-3) : number
+  }
+
   pokemonImgSrc = (pokeId) => {
-    return `${POKE_API}${pokeId}.png`
+    return `${POKE_API}${this.padToThree(pokeId)}.png`
   }
 
   render() {
